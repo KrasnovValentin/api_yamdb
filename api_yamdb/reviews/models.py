4c166d5d541f854d8b05d1d2960cb229b5421df4
Валентин Краснов <valkred@gmail.com>
2022-06-16 09:03:41 +0300

    Krasnov_Categories/Genres/Titles 50%

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from users.models import User
@@ -56,30 +55,32 @@ class Title(models.Model):
         return self.name
 
 
-
 class Review(models.Model):
     text = models.TextField(
         verbose_name='Текст отзыва',
         help_text='Введите текст отзыва'
     )
-    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата отзыва')
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name='Дата отзыва')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='reviews',
         verbose_name='Автор отзыва')
-    title = models.ForeignKey(Titles,
+    title = models.ForeignKey(Title,
                               blank=True,
                               null=True,
                               on_delete=models.SET_NULL,
                               related_name='reviews',
                               verbose_name='Произведение',
-                              help_text='Произведение, к которому относится отзыв')
+                              help_text='Произведение, к '
+                                        'которому относится отзыв')
 
     score = models.IntegerField(
         verbose_name='Рейтинг',
-        validators=[MinValueValidator(1, message='Значение должно быть больше 1'),
-        MaxValueValidator(10, message='Значение должно быть меньше 10')])
+        validators=[
+            MinValueValidator(1, message='Значение должно быть больше 1'),
+            MaxValueValidator(10, message='Значение должно быть меньше 10')])
 
     def __str__(self):
         return self.text[:WORD_COUNT]
@@ -112,7 +113,7 @@ class Comment(models.Model):
         related_name='comments',
         verbose_name='Отзыв',
         help_text='Отзыв, к которому относится комментарий')
-    
+
     class Meta:
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
